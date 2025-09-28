b27e184286d4269652a9750fe9bc07e78d053c10
Neil Enns <neile@live.com>
2025-09-28 10:52:33 -0700

    Upgrade to new eslint version (#29)
    Fixes #28

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { app, BrowserWindow, screen, session } from "electron";
 import WinState from "electron-win-state";
 import Store from "electron-store";
+import squirrelStartup from "electron-squirrel-startup";
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require("electron-squirrel-startup")) {
+if (squirrelStartup) {
   app.quit();
 }
 
